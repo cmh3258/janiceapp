9e535246ee3ce271a7549670caa047b6b589819d
chris <chume@gmail.com>
2017-09-26 22:31:09 -0500

    npm start working

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
   	res.writeHead(200);
-  	// res.end('Hello Http');
   	var file = fs.createReadStream('index.html');
   	file.pipe(res);
+  	// res.end('Hello Http');
 });
 server.listen(8080);
 
